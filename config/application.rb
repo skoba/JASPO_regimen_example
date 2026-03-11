6ceabe55c5eff8899e7d8112a02e241e7ad99e05
KOBAYASHI Shinji <skoba@moss.gr.jp>
2026-03-11 15:21:40 +0900

    Rename project to JASPO Regimen Master

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require 'action_view/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsRegimenMaster
+module JaspoRegimenMaster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
